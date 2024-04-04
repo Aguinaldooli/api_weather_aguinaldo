455c3130d25ee4ecfb9bb5143a670a0e8e548e37
Aguinaldo <guiga_oli@hotmail.com>
2024-04-04 20:47:42 -0300

    first attempt to resolve connection with the bank when it is offline

--- weather/views.py
+++ weather/views.py
@@ -6,20 +6,28 @@ from django.shortcuts import render, redirect
 from .models import WeatherEntity
 from .repositories import WeatherRepository
 from .serializers import WeatherSerializer
+from .exceptions import WeatherException
 
 class WeatherView(View):
     def get(self, request):
         verse = main.get_bible_verse()
         repository = WeatherRepository(collectionName='weathers')
-        weathers = list(repository.getAll())
-        serializer = WeatherSerializer(data=weathers, many=True)
-        if (serializer.is_valid()):
-            weathersData = serializer.data
-            print(serializer.data)
-        else:
-            print(serializer.errors)
-        return render(request, "home.html", {"weathers":weathersData, "verse":verse})
+        try:
+            weathers = list(repository.getAll())
+            serializer = WeatherSerializer(data=weathers, many=True)
+            if (serializer.is_valid()):
+                modelWeather = serializer.save()
+                print(serializer.data)
+            else:
+                print(serializer.errors)
+            objectReturn = {"weathers":modelWeather, "verse":verse}
+        except WeatherException:
+            objectReturn = {"error":str(WeatherException), "verse":verse}
+
+        print (objectReturn)
+        return render(request, "home.html", objectReturn)
     
+            
 
 class WeatherGenerate(View):
     def get(self, request):
@@ -29,16 +37,16 @@ class WeatherGenerate(View):
             date=datetime.now()
         )
         serializer = WeatherSerializer(data=weather.__dict__)
-        if (serializer.is_valid()):
-            repository.insert(serializer.data)
+        if serializer.is_valid():
+            repository.insert(serializer.validated_data)
         else:
             print(serializer.errors)
 
-        return redirect('Weather View')
+        return redirect('weather-view')
     
 class WeatherReset(View):
-    def get(self, request): 
+    def get(self, request):
         repository = WeatherRepository(collectionName='weathers')
         repository.deleteAll()
 
-        return redirect('Weather View')
+        return redirect('weather-view')
